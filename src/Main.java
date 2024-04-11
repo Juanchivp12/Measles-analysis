b4dc6c41c3c8ca2a49fc1ab3aa8689781eafd6f5
Juanchivp12 <Juanvillegasse@gmail.com>
2024-04-11 18:13:49 -0500

    just changed the name of main file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class MeaslesTest
+public class Main
   {
     public static void main(String[] args) throws IOException
     {
