82ffdb35d1c7e323b1857bfc12a0e9a95dcd8b4d
Juanchivp12 <Juanvillegasse@gmail.com>
2024-04-11 17:32:51 -0500

    Added a few things
    Started the project. I think this is a good start. I added a method that just prints all the codons in each file depending on what RF the user chooses. I think this will help get us started

---
+++ src/AminoAcid.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class AminoAcid
+{
+    private String fullName = " ";
+    private String abbreviation = " ";
+    private ArrayList<String> codons = new ArrayList<String>();
+
+    // CONSTRUCTORS
+    public AminoAcid() {}
+
+    public AminoAcid(String fullName, String abbreviation, ArrayList<String> codons)
+    {
+        this.fullName = fullName;
+        this.abbreviation = abbreviation;
+        this.codons = codons;
+    }
+
+    // SETTERS
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
+
+    public void setAbbreviation(String abbreviation)
+    {
+        this.abbreviation = abbreviation;
+    }
+
+    public void setCodons(ArrayList<String> codons)
+    {
+        this.codons = codons;
+    }
+
+    // GETTERS
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public String getAbbreviation()
+    {
+        return abbreviation;
+    }
+
+    public ArrayList<String> getCodons()
+    {
+        return codons;
+    }
+
+    // toString, equals, and clone methods
+    public String toString()
+    {
+        return "";
+    }
+
+    public boolean equals(AminoAcid aminoAcidTest)
+    {
+        return this.fullName == aminoAcidTest.fullName
+                && this.abbreviation == aminoAcidTest.abbreviation
+                && this.codons == aminoAcidTest.codons;
+    }
+
+    public AminoAcid clone()
+    {
+        return new AminoAcid(fullName, abbreviation, codons);
+    }
+}
--- src/MeaslesTest.java
+++ src/MeaslesTest.java
@@ -1,7 +1,60 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class MeaslesTest
   {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-      //Testing webhook
+      final int RF1 = 1;
+      final int RF2 = 2;
+      final int RF3 = 3;
+
+      int fileChoice;
+      Scanner kbd = new Scanner(System.in);
+
+      do
+      {
+      System.out.print("Which RF file would you like to scan? (1, 2, 3) ");
+      fileChoice = kbd.nextInt();
+      }
+      while (fileChoice < RF1 || fileChoice > RF3);
+
+      switch (fileChoice)
+      {
+        case RF1:
+        fileReader("measlesSequenceRF1.csv");
+        break;
+
+        case RF2:
+        fileReader("measlesSequenceRF2.csv");
+        break;
+        
+        case RF3:
+        fileReader("measlesSequenceRF3.csv");
+        break;
+      }
+    }
+
+    /**
+     * This method reads through any of the 3 reading frames
+     * @param fileName a String representing a reading frame
+     * @throws IOException
+     */
+    public static void fileReader(String fileName) throws IOException
+    {
+        File file = new File(fileName);
+        Scanner inFile = new Scanner(file);
+
+        while (inFile.hasNext())
+        {
+            String str = inFile.nextLine();
+            String[] tokens = str.split(",");
+
+            for (String s : tokens)
+            {
+                System.out.print(s + " ");
+            }
+        }
+        inFile.close();
     }
   }
